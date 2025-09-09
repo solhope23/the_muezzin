3f3dd1e63435487d055a3ea0a2f2b4c13c567c69
solhope23 <s526413103@gmail.com>
2025-09-09 09:42:15 +0300

    audio_file_reader

--- servises/step_a/step_a_manager/step_a_manager.py
+++ servises/step_a/step_a_manager/step_a_manager.py
@@ -15,7 +15,7 @@ class StepAManager:
 
     def manage(self):
         try:
-            gen_files_path_object = FileReader.get_path_object_of_file(self.data_directory_path)
+            gen_files_path_object = FileReader.read_file_path_object_gen(self.data_directory_path)
             gen_metadata_objects = DataBuilder.generator_metadata_to_metadata_object(gen_files_path_object)
             with KafkaProducerConnection(self.configs_producer) as kafka_client:
                 producer = MyKafkaProducer(kafka_client)
